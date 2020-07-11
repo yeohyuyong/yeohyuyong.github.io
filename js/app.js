1dd887844a532fff274cd6163936d16b93a507e5
Yeoh Yu Yong <yeoh.yuyong@gmail.com>
2020-07-11 10:39:23 +0800

    chore: Delete app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', moveScrollIndicator);
-
-const scrollIndicatorElt = document.getElementById('scrollIndicator');
-const maxHeight = window.document.body.scrollHeight - window.innerHeight;
-
-function moveScrollIndicator(e) {
-  const percentage = ((window.scrollY) / maxHeight) * 100;
-  scrollIndicatorElt.style.width = percentage + '%';
-}
